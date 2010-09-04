e3cb0450cab49ede190df081d502d0ea64edfc5e
Andreas Rottmann <a.rottmann@gmx.at>
2010-09-04 23:41:59 +0200

    pkg-list.scm: expand SRFI dependency, add descriptive metadata

--- pkg-list.scm
+++ pkg-list.scm
@@ -1,5 +1,23 @@
-(package (industria (0))
-  (depends (srfi))
+(package (industria (0) (2010 8 25) (1))
+  (depends (srfi-1)
+           (srfi-13)
+           (srfi-14)
+           (srfi-19)
+           (srfi-26)
+           (srfi-27)
+           (srfi-39))
+  
+  (synopsis "Göran Weinholt's library collection")
+  (description
+   "Industria contains a set of libraries covering:"
+   " - Networking protocols: TCP (client only), TLS, DNS, IRC, OTR"
+   " - Cryptography: AES, ARC4, MD5, SHA-{1,2}, Blowfish, HMAC, CRC, DES"
+   " - Text encodings: base64, punycode"
+   " - Compression: ZIP, gzip"
+   " - Bytevector pack/unpack syntax"
+   " - An amd64 disassembler")
+  (homepage "https://code.launchpad.net/~weinholt/scheme-libraries/industria")
+  
   (libraries "weinholt"))
 
 ;; Local Variables:
